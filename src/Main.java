9031c28aff43e3ca69f909e3cc7fef1e803cbd82
EmmyAKEHURST <emmy.akehurst@gmail.com>
2024-11-23 15:10:44 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Modele.connexionBdd();
+		//test
+		
 		if (Modele.connexionUtilisateur("eMusk", Fonction.hashMD5("Azerty123")) == true) {
 			System.out.println("Correcte");
 		}
 		else {
 			System.out.println("Incorrecte");
 		}
+		
+		
 		Modele.fermetureBdd();
 
 	}
