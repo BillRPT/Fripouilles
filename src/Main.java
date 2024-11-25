bbdc5d0ce456b2d85b037bf811b755d66d9ec356
EmmyAKEHURST <emmy.akehurst@gmail.com>
2024-11-25 21:26:52 +0100

    miaou

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Modele.connexionBdd();
-		//je modifie le main si t'arrives pas à pull je pense que je pète mon crâneeqeqzez
+		//On va y arriver, j'y crois.
 		
 		if (Modele.connexionUtilisateur("eMusk", Fonction.hashMD5("Azerty123")) == true) {
 			System.out.println("Correcte");
